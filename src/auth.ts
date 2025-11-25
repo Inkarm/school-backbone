6ca14f3d3db7a25ac372bbeaab156fb7cea57cfa
Inkarm <117416876+Inkarm@users.noreply.github.com>
2025-11-26 00:05:53 +0100

    Fix malformed auth.ts file

--- src/auth.ts
+++ src/auth.ts
@@ -20,43 +20,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
         Credentials({
             async authorize(credentials) {
-                import NextAuth from 'next-auth';
-                import Credentials from 'next-auth/providers/credentials';
-                import { authConfig } from './auth.config';
-                import { z } from 'zod';
-                import prisma from '@/lib/prisma';
-                import bcrypt from 'bcryptjs';
+                const parsedCredentials = z
+                    .object({ login: z.string(), password: z.string().min(6) })
+                    .safeParse(credentials);
 
-                async function getUser(login: string) {
-                    try {
-                        const user = await prisma.user.findUnique({ where: { login } });
-                        return user;
-                    } catch (error) {
-                        console.error('Failed to fetch user:', error);
-                        throw new Error('Failed to fetch user.');
-                    }
+                if (parsedCredentials.success) {
+                    const { login, password } = parsedCredentials.data;
+                    const user = await getUser(login);
+                    if (!user) return null;
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+                    if (passwordsMatch) return { ...user, id: user.id.toString() };
                 }
 
-                export const { handlers, auth, signIn, signOut } = NextAuth({
-                    ...authConfig,
-                    providers: [
-                        Credentials({
-                            async authorize(credentials) {
-                                const parsedCredentials = z
-                                    .object({ login: z.string(), password: z.string().min(6) })
-                                    .safeParse(credentials);
-
-                                if (parsedCredentials.success) {
-                                    const { login, password } = parsedCredentials.data;
-                                    const user = await getUser(login);
-                                    if (!user) return null;
-
-                                    const passwordsMatch = await bcrypt.compare(password, user.password);
-                                    if (passwordsMatch) return { ...user, id: user.id.toString() };
-                                }
-
-                                return null;
-                            },
-                        }),
-                    ],
-                });
+                return null;
+            },
+        }),
+    ],
+});
